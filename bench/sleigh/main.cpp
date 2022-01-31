cd7aae7b9e9a2eedab505bcdee5cb48a175a4296
Alex Cameron <alex.cameron@trailofbits.com>
2022-01-31 11:39:41 +0000

    Cleanup and format SLEIGH benchmark

--- bench/sleigh/main.cpp
+++ bench/sleigh/main.cpp
@@ -1,10 +1,8 @@
-#include "_deps/ghidrasource-src/Ghidra/Features/Decompiler/src/decompile/cpp/translate.hh"
 extern "C"
 {
 #include "load_bin.h"
 }
 #include <sleigh/libsleigh.hh>
-#include <iostream>
 #include <cassert>
 
 namespace
@@ -13,24 +11,14 @@ namespace
 class InMemoryLoadImage : public LoadImage
 {
 public:
-    explicit InMemoryLoadImage()
-        : LoadImage("nofile") {}
-
-    void SetImageBuffer(std::string &&buf)
-    {
-        assert(image_buffer.empty());
-        image_buffer = std::move(buf);
-    }
+    explicit InMemoryLoadImage(std::string&& image_buffer)
+        : LoadImage("nofile"), image_buffer(std::move(image_buffer)) {}
 
     void loadFill(unsigned char *ptr, int size, const Address &addr) override
     {
         uint64_t start = addr.getOffset();
-        for (int i = 0; i < size; ++i)
-        {
-            uint64_t offset = start + i;
-            assert(offset < image_buffer.size());
-            ptr[i] = image_buffer[offset];
-        }
+        assert(start + size <= image_buffer.size());
+        memcpy(ptr, &image_buffer[start], size);
     }
 
     std::string getArchType(void) const override
@@ -44,44 +32,42 @@ private:
     std::string image_buffer;
 };
 
-class AssemblyPrinter : public AssemblyEmit
+class AssemblyNoop : public AssemblyEmit
 {
 public:
     void dump(const Address &, const std::string &,
-              const std::string &) override
-    {
-        // No impl
-    }
+              const std::string &) override {}
 };
 
 } // namespace
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    uint8_t* code = nullptr;
+    uint8_t *code = nullptr;
     size_t code_len = 0, loop_count = 0;
     if (!read_file(argc, argv, &code, &code_len, &loop_count))
     {
         return 1;
     }
-
-    std::string image_buffer(reinterpret_cast<char*>(code), code_len);
+    std::string image_buffer(reinterpret_cast<char *>(code), code_len);
 
     // Initialize SLEIGH
-    InMemoryLoadImage load_image;
-    load_image.SetImageBuffer(std::move(image_buffer));
+    InMemoryLoadImage load_image(std::move(image_buffer));
     ContextInternal ctx;
     Sleigh engine(&load_image, &ctx);
     DocumentStorage storage;
     const auto sla_path = sleigh::FindSpecFile("x86-64.sla");
     if (!sla_path)
     {
-        std::cerr << "Couldn't find sla file\n";
+        fputs("Couldn't find sla file\n", stderr);
+        free(code);
         return 1;
     }
-    Element* sla_doc = storage.openDocument(*sla_path)->getRoot();
+    Element *sla_doc = storage.openDocument(*sla_path)->getRoot();
     storage.registerTag(sla_doc);
-    Element* pspec_doc = storage.openDocument("/home/ec2-user/Code/disas-bench/bench/sleigh/_deps/ghidrasource-src/Ghidra/Processors/x86/data/languages/x86-64.pspec")->getRoot();
+    Element *pspec_doc = storage.openDocument(
+        "bench/sleigh/_deps/ghidrasource-src/Ghidra/Processors/x86/data/languages/x86-64.pspec"
+        )->getRoot();
     storage.registerTag(pspec_doc);
     engine.initialize(storage);
     engine.allowContextSet(false);
@@ -93,7 +79,7 @@ int main(int argc, char* argv[])
     if (el)
     {
         const List &list(el->getChildren());
-        for (const auto* iter : list)
+        for (const auto *iter : list)
         {
             const string &elname(iter->getName());
             if (elname == "context_data")
@@ -104,11 +90,11 @@ int main(int argc, char* argv[])
         }
     }
 
-    AssemblyPrinter asm_emit;
+    AssemblyNoop asm_emit;
     size_t num_valid_insns = 0, num_bad_insn = 0;
     clock_t start_time = clock();
     Address end_addr(engine.getDefaultCodeSpace(), code_len);
-    for (int i = 0; i < loop_count; ++i)
+    for (size_t round = 0; round < loop_count; ++round)
     {
         Address cur_addr(engine.getDefaultCodeSpace(), 0);
         while (cur_addr < end_addr)
@@ -121,8 +107,8 @@ int main(int argc, char* argv[])
             }
             catch (BadDataError&)
             {
-                ++num_bad_insn;
                 cur_addr = cur_addr + 1;
+                ++num_bad_insn;
             }
         }
     }
