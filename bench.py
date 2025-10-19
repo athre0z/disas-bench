39758f68708a399257ccf1adecdacae4f83098ec
iximeow <me@iximeow.net>
2025-10-19 06:07:47 +0000

    armv7 benchmarks

--- bench.py
+++ bench.py
@@ -9,42 +9,99 @@ import re
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 
-code_filename = os.path.join(root_dir, 'input', 'xul.dll')
-file_code_offs = 0x400
-file_code_len = 0x2460400
-code_loop_count = 20
+x86_bench_defaults = {
+        "arch": "x86",
+        "filename": os.path.join(root_dir, 'input', 'xul.dll'),
+        "file_code_offs": 0x400,
+        "file_code_len": 0x2460400,
+        "code_loop_count": 20,
+}
 
-if len(sys.argv) == 1:
-    pass
-elif len(sys.argv) == 4 or len(sys.argv) == 5:
-    file_code_offs = int(sys.argv[1])
-    file_code_len = int(sys.argv[2])
-    code_filename = sys.argv[3]
-    if len(sys.argv) >= 5:
-        code_loop_count = int(sys.argv[4])
-    else:
-        # Match the old file + loop-count
-        code_loop_count = round(0x2460400 * 20 / file_code_len)
-else:
-    print('Expected no args or:')
-    print(f'  {sys.argv[0]} <code-offset> <code-len> <filename> [loop-count]')
+armv7_bench_defaults = {
+        "arch": "armv7",
+        "filename": os.path.join(root_dir, 'input', 'tdfx_dri.so'),
+        "file_code_offs": 0x17ba8,
+        "file_code_len": 0x1bba4c,
+        "code_loop_count": 2000,
+}
+
+def parse_args():
+    # Argument parsing here is super hacky.. Want to accept command lines like:
+    # # oldest
+    # ./bench.py 0x400 0x2460400 input/xul.dll
+    # # slightly newer: accepts loop count
+    # ./bench.py 0x400 0x2460400 input/xul.dll 20
+    # # input/xul.dll is in the repo, so we have defaults for using it
+    # ./bench.py
+    # # we have ARM defaults and a few ARM bench targets
+    # ./bench.py -a armv7
+    # # and even then, we can provide parameters explicitly
+    # ./bench.py -a armv7 0x17ba8 0x1bba4c input/rdfx_dri.so 2001
+    # # finally, explicitly picking the old behavior should work
+    # ./bench.py -a x86 0x400 0x2460400 ./input/xul.dll 20
+    #
+    # so: the plan is to pick out a `-a` if we see it, and from there if there are
+    # zero arguments, we're done. if there are four arguments, it's the old style
+    # and pick an implicit loop count. and if there are five arguments, everything
+    # is specified and `-a` if present just selects bench targets.
+
+    if len(sys.argv) == 1:
+        # no argument, return None and hope usage helps.
+        return None
+
+    bench_settings = None
+
+    if len(sys.argv) > 2 and sys.argv[1] == "-a":
+        # ./bench.py -a foo
+        # consume both arguments, continue onward
+        arch = sys.argv[2]
+        del sys.argv[2]
+        del sys.argv[1]
+
+        if arch == "x86":
+            bench_settings = x86_bench_defaults
+        elif arch == "armv7":
+            bench_settings = armv7_bench_defaults
+        else:
+            print(f"Unknown architecture: {arch}")
+            return None
+
+
+    if len(sys.argv) == 4 or len(sys.argv) == 5:
+        bench_settings["file_code_offs"] = int(sys.argv[1])
+        bench_settings["file_code_len"] = int(sys.argv[2])
+        bench_settings["filename"] = sys.argv[3]
+        if len(sys.argv) >= 5:
+            bench_settings["code_loop_count"] = int(sys.argv[4])
+        else:
+            # default already set in bench_settings.
+            pass
+
+    return bench_settings
+
+bench_settings = parse_args()
+
+if not bench_settings:
+    print(f'  {sys.argv[0]} [-a {{x86,armv7}}] [<code-offset> <code-len> <filename> [loop-count]]')
+    print(f'')
+    print(f'  For example: {sys.argv[0]} -a x86 0x1000 0x4000 your-benchmark.dll 65536')
     sys.exit(1)
 
-if not os.path.exists(code_filename):
-    print(f'File `{code_filename}` does not exist')
+if not os.path.exists(bench_settings["filename"]):
+    print(f'File `{bench_settings["filename"]}` does not exist')
     sys.exit(1)
-if file_code_offs < 0:
-    print(f'Invalid code-offset {file_code_offs}')
+if bench_settings["file_code_offs"] < 0:
+    print(f'Invalid code-offset {bench_settings["file_code_offs"]}')
     sys.exit(1)
-if file_code_len < 0:
-    print(f'Invalid code-len {file_code_len}')
+if bench_settings["file_code_len"] < 0:
+    print(f'Invalid code-len {bench_settings["file_code_len"]}')
     sys.exit(1)
-if code_loop_count < 0:
-    print(f'Invalid loop-count {code_loop_count}')
+if bench_settings["code_loop_count"] < 0:
+    print(f'Invalid loop-count {bench_settings["code_loop_count"]}')
     sys.exit(1)
 
 
-targets = [
+targets_x86 = [
     # Decode + format
     ['bench/cs/bench-cs-fmt', 'Capstone decode+fmt'],
     ['bench/zydis/bench-zydis-full-fmt', 'Zydis (full) decode+fmt'],
@@ -64,7 +121,20 @@ targets = [
     ['bench/bddisasm/bench-bddisasm-no-fmt', 'bddisasm decode'],
     ['bench/yaxpeax/bench-yaxpeax-no-fmt', 'yaxpeax decode'],
 ]
+targets_armv7 = [
+    # Decode + format
+    ['bench/yaxpeax-arm/bench-yaxpeax-fmt', 'yaxpeax-arm decode+fmt'],
+
+    # Decode only
+    ['bench/yaxpeax-arm/bench-yaxpeax-no-fmt', 'yaxpeax-arm decode'],
+]
+
 timings = []
+
+targets = {
+        "x86": targets_x86,
+        "armv7": targets_armv7
+}[bench_settings["arch"]]
 
 if platform.system() == 'Windows':
     for x in targets:
@@ -73,7 +143,7 @@ if platform.system() == 'Windows':
 assert all(os.path.exists(x[0]) for x in targets)
 
 # Open & read file once before to make sure it's in OS cache.
-with open(code_filename, 'rb') as f:
+with open(bench_settings["filename"], 'rb') as f:
     f.read()
 
 print('[*] Performing benchmarks')
@@ -85,10 +155,10 @@ for cur_target in targets:
 
     process_args = [
         os.path.join(root_dir, cur_target[0]),
-        f'0x{code_loop_count:X}',
-        f'0x{file_code_offs:X}',
-        f'0x{file_code_len:X}',
-        code_filename
+        f'0x{bench_settings["code_loop_count"]:X}',
+        f'0x{bench_settings["file_code_offs"]:X}',
+        f'0x{bench_settings["file_code_len"]:X}',
+        bench_settings["filename"]
     ]
     prev = time.time()
     process = subprocess.run(process_args, stdout=subprocess.PIPE)
@@ -97,7 +167,7 @@ for cur_target in targets:
     if process.returncode != 0:
         raise ValueError(f'{cur_target[0]} exited with code {process.returncode}')
     output = process.stdout.decode('utf-8')
-    m = re.search('Disassembled (\d+) instructions \((\d+) valid, (\d+) bad\), (\S+) ms', output)
+    m = re.search(r'Disassembled (\d+) instructions \((\d+) valid, (\d+) bad\), (\S+) ms', output)
     if m is None:
         raise ValueError(f"Couldn't parse output: `{output}`")
     groups = m.groups()
@@ -110,8 +180,8 @@ for cur_target in targets:
 
 print('[*] Generating chart')
 
-code_len_mb = file_code_len / 1024 / 1024
-mbs = [(code_len_mb * code_loop_count / x) for x in timings]
+code_len_mb = bench_settings["file_code_len"] / 1024 / 1024
+mbs = [(code_len_mb * bench_settings["code_loop_count"] / x) for x in timings]
 plt.rcdefaults()
 fig = plt.figure(figsize=(10, 5))
 ax = fig.add_subplot(1, 1, 1)
